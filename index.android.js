bdc5035673b95f73ed80ffa4859833ddff4d5bae
Andika Raditya <Andika Raditya>
2017-04-11 09:31:38 +0800

    test

---
+++ index.android.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import mainApp from './src/mainApp';
+
+AppRegistry.registerComponent('tip3', () => mainApp);
